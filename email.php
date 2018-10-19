e414918b58bc35d20883c69ea1dc8d9f7d22ac68
Edsel Norwood <edselnorwood83@gmail.com>
2018-10-19 17:59:38 -0400

    Update email.php
    Removed extra $_POST fields

--- email.php
+++ email.php
@@ -1,3 +1,3 @@
 <?php
-    mail('edselnorwood83@gmail.com', $_POST['email'], $_POST['name'], $_POST['subject'], $_POST['message']);
+    mail('edselnorwood83@gmail.com',$_POST['subject'], $_POST['message']);
 ?>
